083ce9e250ba86078bb383e0aa2d278280d1adc2
cerisilv123 <cerisilv@hotmail.co.uk>
2024-01-01 19:48:13 +0000

    Added class response. response class is an object that can be instantiated to return a JSON error or success response ensuring consistency in formatting of responses.

---
+++ app/response.py
@@ -0,0 +1,55 @@
+from flask import jsonify
+
+class Response:
+    def __init__(self, message, data, status_code=200):
+        """
+        Constructor for initializing the class with a message, data, and an optional status code.
+        Success responses will always have status code 200 so it is set by default.
+
+        Args:
+            message (str): Message associated with the response.
+            data (Any): Data payload of the response.
+            status_code (int, optional): HTTP status code (default is 200).
+        """
+        self.message = message
+        self.data = data
+        self.status_code = status_code
+
+    def return_error_response(self, status_code): 
+        """
+        Helper function to generate JSON error response.
+
+        Parameters:
+            error_message (str): The error message to be included in the response.
+            status_code (int): The HTTP status code for the error.
+
+        Returns:
+            tuple: A tuple containing the JSON error response and the appropriate HTTP status code.
+        """
+        response = {
+            'error': {
+                'message': self.message,
+                'status_code': status_code
+            }
+        }
+        return jsonify(response), status_code
+    
+    def return_success_response(self):
+        """
+        Helper function to generate JSON success response.
+
+        Parameters:
+            success_message (str): The success message to be included in the response.
+            data (list, optional): Additional data to be included in the response (default is an empty list).
+
+        Returns:
+            tuple: A tuple containing the JSON success response and the HTTP status code 200.
+        """
+        response = {
+            'success': {
+                'message': self.message, 
+                'status_code': self.status_code,
+                'data': self.data,
+            }
+        }
+        return jsonify(response), 200
